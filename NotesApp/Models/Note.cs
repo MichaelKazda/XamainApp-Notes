d2d965051a6810c8b7fa5ff53fb97bd21db0923b
Michael Kazda <michaelkazda@seznam.cz>
2020-10-19 16:11:20 +0200

    DB setup

---
+++ NotesApp/Models/Note.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotesApp.Models {
+    public class Note {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public DateTime DateCreate { get; set; }
+        public DateTime DateUpdate { get; set; }
+    }
+}
